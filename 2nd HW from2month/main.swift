f9f49f427210c42c239e09a97378ca2094645d65
nissinissinissi <nissisalamat14@icloud.com>
2022-08-31 07:40:49 -0500

    class

--- 2nd HW from2month/main.swift
+++ 2nd HW from2month/main.swift
@@ -7,5 +7,68 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+   var name: String = ""
+   var surname : String = ""
+   var age: Int = 0
 
+   init(name: String, surname: String, age: Int) {
+       self.name = name
+       self.surname = surname
+       self.age = age
+   }
+       func info() -> String
+   {
+           return("name: \(self.name),  surname: \(self.surname), age: \(self.age)")
+       }
+   }
+var pers: Person = Person(name: "Okeanna", surname: "Abel", age: 22)
+print (pers.info())
+
+class Employee : Person{
+    var company: String
+   var title : String
+   init(name: String, surname: String, company: String, age: Int, title: String){
+       self.title = title
+       self.company = company
+       super.init(name: name, surname: surname, age: age)
+   }
+    
+   func getFullInfo() -> String{
+   
+       return "name: \(self.name),  surname: \(self.surname), age: \(self.age), working at \(self.company) as a: \(self.title) "
+   }
+}
+
+var emp: Employee = Employee(name: "Adam", surname: "Binance", company:"Apple", age: 32, title: "producer")
+print(emp.getFullInfo())
+
+class investor : Employee{
+
+   var amount: Int = 0
+   
+
+   init(name: String, surname: String, company: String, age: Int, title: String, amount : Int){
+       super.init(name: name, surname: surname, company: company, age: age, title: title)
+    self.amount = amount
+   }
+       func showAmount() {
+           print ("name: \(self.name),  surname: \(self.surname), age \(self.age)  working at \(self.company) as a: \(self.title), invest is : \(self.amount) USA. ")
+       }
+   }
+var Inv: investor = investor(name: "Amir", surname: "Nurbekov", company: "Amazon", age: 29, title:"procurement", amount: 123450 )
+print (Inv.showAmount())
+
+var company = "Google"
+var ammount = 3489
+var invests = "10000"
+print ("Company#1 \(company) , the number of employeers: \(ammount),  the ivest is : \(invests)USD")
+
+var secondCom = "Apple"
+var empl = 234
+var inve = "39097"
+print ("Company#2 \(secondCom) the number of emoployeers : \(empl), and the invest is : \(inve)USD")
+var third = "Amazon"
+var employ = 12345
+var inves = "345324"
+print ("Company#3 \(third) the number of employeers \(employ), and invest is: \(123450)" )
